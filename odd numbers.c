2e0fae73bbbc7eeda450bea70a00556190ec441e
Nizar Beriane <nizarberiane43@gmail.com>
2024-09-11 15:53:40 +0100

    fix

--- odd numbers.c
+++ odd numbers.c
@@ -5,10 +5,11 @@ int sum = 0;
 int main() {
     printf("Enter the number: ");
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n;) {
         sum += 1;
         if ((sum % 2)!= 0) {
             printf("%d ", sum);
+            i++;
         }
     }
 }
